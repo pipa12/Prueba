2d41a238fb4f339d0b6aa5f68f1d1d8eee19a47e
cd.. <lulo.ospino@gmail.com>
2018-07-31 06:46:06 -0500

    Poner commit de prueba al repositorio remoto

--- src/ayuda/Ayuda.java
+++ src/ayuda/Ayuda.java
@@ -17,6 +17,7 @@ public class Ayuda {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("AYUDA");
+        System.err.println("Error");
     }
     
 }
